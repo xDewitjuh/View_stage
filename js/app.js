c27ed54b4b47da25cc5d8596ffd9c70a9d588489
xDewitjuh <xdewitjuhx@gmail.com>
2026-06-22 13:11:43 +0200

    Implement dashboard KPI cards

--- js/app.js
+++ js/app.js
@@ -7,6 +7,15 @@ const siteSelect = document.getElementById("site-select");
 const currentMonth = document.getElementById("currentMonth");
 const prevMonth = document.getElementById("prevMonth");
 const nextMonth = document.getElementById("nextMonth");
+const consumptionCard = document.getElementById("consumptionCard");
+const usageCard = document.getElementById("usageCard");
+const totalCostCard = document.getElementById("totalCostCard");
+const rateCard = document.getElementById("rateCard");
+const costPersonCard = document.getElementById("costPersonCard");
+const totalCarbonCard = document.getElementById("totalCarbonCard");
+const carbonPersonCard = document.getElementById("carbonPersonCard");
+const treeCard = document.getElementById("treeCard");
+
 
 async function loadData() {
     const response = await fetch("./data/data.json");
@@ -74,6 +83,39 @@ function updateDashboard() {
     console.log("Selected month:", monthKey);
 
     const data = dashboardData[selectedSite]?.[monthKey];
+    if (!data) return;
+
+    consumptionCard.innerHTML = `
+    <h3>Electricity Consumption</h3>
+    <p>${data.electricityConsumption.toLocaleString()} kWh</p>`;
+
+    usageCard.innerHTML = `
+    <h3>Usage Intensity</h3>
+    <p>${data.usageIntensity}</p>`;
+
+    totalCostCard.innerHTML = `
+    <h3>Total Cost</h3>
+    <p>£${data.totalCost}</p>`;
+
+    rateCard.innerHTML = `
+    <h3>Daily Rate</h3>
+    <p>£${data.dailyRate}</p>`;
+
+    costPersonCard.innerHTML = `
+    <h3>Cost Per Person</h3>
+    <p>£${data.costPerPerson}</p>`;
+
+    totalCarbonCard.innerHTML = `
+    <h3>Carbon Total</h3>
+    <p>${data.carbonTotal}</p>`;
+
+    carbonPersonCard.innerHTML = `
+    <h3>Carbon Per Person</h3>
+    <p>${data.carbonPerPerson}</p>`;
+
+    treeCard.innerHTML = `
+    <h3>Trees Planted</h3>
+    <p>${data.treesPlanted}</p>`;
 
     console.log(data);
 }
